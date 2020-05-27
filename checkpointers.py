9e092fe610d5d611a80bf582f3c4c8373c725148
Ducvoccer <ntduc250798@gmail.com>
2020-05-27 21:54:31 +0700

    config model checkpoint

--- checkpointers.py
+++ checkpointers.py
@@ -8,101 +8,11 @@ import io
 from collections import OrderedDict
 from collections import Iterable
 
-from .utils import *
-from .accuracy import *
 from time import time
 from tensorflow.python.lib.io import file_io
-from .datagen import ImageDataGeneratorCustom
 import logging
 
 
-class CSVLogger(tf.keras.callbacks.Callback):
-    """Callback that streams epoch results to a csv file.
-    Supports all values that can be represented as a string,
-    including 1D iterables such as np.ndarray.
-    # Example
-    ```python
-    csv_logger = CSVLogger('training.log')
-    model.fit(X_train, Y_train, callbacks=[csv_logger])
-    ```
-    # Arguments
-        filename: filename of the csv file, e.g. 'run/log.csv'.
-        separator: string used to separate elements in the csv file.
-        append: True: append if file exists (useful for continuing
-            training). False: overwrite existing file,
-    """
-
-    def __init__(self, job_dir, filepath, separator=',', append=False):
-        self.sep = separator
-        self.append = append
-        self.filepath = filepath
-        self.writer = None
-        self.keys = None
-        self.job_dir = job_dir
-        self.append_header = True
-        if six.PY2:
-            self.file_flags = 'b'
-            self._open_args = {}
-        else:
-            self.file_flags = ''
-            self._open_args = {'newline': '\n'}
-        super(CSVLogger, self).__init__()
-
-    def on_train_begin(self, logs=None):
-        if self.append:
-            if os.path.exists(self.filepath):
-                with open(self.filepath, 'r' + self.file_flags) as f:
-                    self.append_header = not bool(len(f.readline()))
-            mode = 'a'
-        else:
-            mode = 'w'
-        self.csv_file = io.open(self.filepath,
-                                mode + self.file_flags,
-                                **self._open_args)
-
-    def on_epoch_end(self, epoch, logs=None):
-        logs = logs or {}
-
-        def handle_value(k):
-            is_zero_dim_ndarray = isinstance(k, np.ndarray) and k.ndim == 0
-            if isinstance(k, six.string_types):
-                return k
-            elif isinstance(k, Iterable) and not is_zero_dim_ndarray:
-                return '"[%s]"' % (', '.join(map(str, k)))
-            else:
-                return k
-
-        if self.keys is None:
-            self.keys = sorted(logs.keys())
-
-        if self.model.stop_training:
-            # We set NA so that csv parsers do not fail for this last epoch.
-            logs = dict([(k, logs[k] if k in logs else 'NA') for k in self.keys])
-
-        if not self.writer:
-            class CustomDialect(csv.excel):
-                delimiter = self.sep
-            fieldnames = ['epoch'] + self.keys
-            if six.PY2:
-                fieldnames = [unicode(x) for x in fieldnames]
-            self.writer = csv.DictWriter(self.csv_file,
-                                         fieldnames=fieldnames,
-                                         dialect=CustomDialect)
-            if self.append_header:
-                self.writer.writeheader()
-
-        row_dict = OrderedDict({'epoch': epoch})
-        row_dict.update((key, handle_value(logs[key])) for key in self.keys)
-        self.writer.writerow(row_dict)
-        self.csv_file.flush()
-
-        if self.job_dir.startswith("gs://"):
-            if os.path.exists(self.filepath):
-                backup_file(self.job_dir, self.filepath)
-
-    def on_train_end(self, logs=None):
-        self.csv_file.close()
-        self.writer = None
         
         
 class ModelCheckpoint(tf.keras.callbacks.Callback):
@@ -203,46 +113,3 @@ class ModelCheckpoint(tf.keras.callbacks.Callback):
                     self.model.save(filepath, overwrite=True)
      
 
-
-class TestAccuracy(tf.keras.callbacks.Callback):
-
-    class DataGenerator(object):
-        def __init__(self, params, data_path, target_size=(224, 224)):
-            self.params = params
-            self.target_size = target_size
-            self.idg = ImageDataGeneratorCustom(**params)
-            self.data_path = data_path
-
-        def get_test_generator(self, batch_size):
-            if not os.path.exists('tops_test.csv'):
-                with file_io.FileIO(self.data_path + 'tops_test.csv', mode='r') as input_f:
-                    with file_io.FileIO('tops_test.csv', mode='w+') as output_f:
-                        output_f.write(input_f.read())
-            return self.idg.flow_from_directory("tops/",
-                                                batch_size=batch_size,
-                                                target_size=self.target_size, shuffle=False,
-                                                triplet_path='tops_test.csv')
-
-    def __init__(self, data_path):
-        self.data_path = data_path
-        super(TestAccuracy, self).__init__()
-
-    def on_train_begin(self, logs={}):
-        dg = self.DataGenerator({
-            "rescale": 1. / 255,
-            "horizontal_flip": True,
-            "vertical_flip": True,
-            "zoom_range": 0.2,
-            "shear_range": 0.2,
-            "rotation_range": 30,
-            "fill_mode": 'nearest' 
-          }, self.data_path, target_size=(224, 224))
-          
-        self.test_generator = dg.get_test_generator(48)
-        
-
-    def on_epoch_end(self, epoch, logs={}):
-        y_pred = self.model.predict_generator(self.test_generator, steps = self.test_generator.n//(self.test_generator.batch_size*10))
-        acc = accuracy_fn(48)([], y_pred)
-        logging.info("Test Accuracy: "+str(acc))
-        return acc
